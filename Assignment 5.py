ef6d582d35300a643aefda29449449f75cb18be9
Shravani Bhosale <shravanibhosale999@gmail.com>
2026-01-05 21:32:41 +0530

    Add files via upload

---
+++ Assignment 5.py
@@ -0,0 +1,46 @@
+# Q1. Display value, type, memory address
+x = input("Enter value: ")
+print(x)
+print(type(x))
+print(id(x))
+
+# Q2. Difference using id()
+a = 10
+b = 10
+print(id(a), id(b))
+
+a = [10]
+b = [10]
+print(id(a), id(b))
+
+# Q5. Predict the output
+a = 10
+b = 10
+print(id(a) == id(b))
+
+# Q5. Predict the output
+a = 10
+b = 10
+print(id(a) == id(b))
+
+# Q6. Program for arithmetic operations
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Q8. Predict the output
+x = input("Enter number: ")
+print(type(x))
+
+# Q9. Program to display name and next year's age
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+print("Hello", name, ", you will turn", age + 1, "next year.")
+
+# Q10. Output difference
+print("10" + "20")
+print(10 + 20)
