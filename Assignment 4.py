a7ef90bfbcc4c93b5d8163b873e0cd5269e72f5f
Shravani Bhosale <shravanibhosale999@gmail.com>
2026-01-05 21:39:48 +0530

    Update Assignment 4.py

---
+++ Assignment 4.py
@@ -0,0 +1,19 @@
+# Assignment 4
+
+# Q2. Bytes output
+b = bytes([65, 66, 67])
+print(b)
+
+# Q4. Bytearray modification
+ba = bytearray([65, 66, 67])
+ba[0] = 97
+print(ba)
+
+# Q6. None type
+x = None
+print(type(x))
+print(x == False)
+
+# Q9. Set example
+s = {10, 20, 30, 10, 20}
+print(s)
